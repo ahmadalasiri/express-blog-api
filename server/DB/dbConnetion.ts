69c038ad7ffc80e9b8e2ece8a0e448dc51570e57
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-08-13 17:31:06 +0300

    Implement MongoDB connection

--- server/DB/dbConnetion.ts
+++ server/DB/dbConnetion.ts
@@ -1,21 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const dbConnection =async () => {
-
-  if (!process.env.DB_URI) { // <-- Add guard clause to check for undefined value
-    console.error("DB_URI environment variable is not defined");
+const dbConnection = async () => {
+  if (!process.env.DB_URI) {
+    // <-- Add guard clause to check for undefined value
+    console.error('DB_URI environment variable is not defined');
     process.exit(1);
-}
-    mongoose.connect(process.env.DB_URI).then((conn) => {
-        console.log(`Database Connected ^_^ ${conn.connection.host} `);
+  }
+  mongoose
+    .connect(process.env.DB_URI as string)
+    .then(conn => {
+      console.log(`Database Connected ✌️ ${conn.connection.host} `);
     })
-    .catch((err) => {
-        console.error(`error: ${err}`);
-        process.exit(1);
+    .catch(err => {
+      console.error(`error: ${err}`);
+      process.exit(1);
     });
 };
 
- 
 export default dbConnection;
-
- 
