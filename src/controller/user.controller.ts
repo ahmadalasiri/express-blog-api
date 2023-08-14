fcd99590838861bf3022f16b8cd08c693be7ceb4
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-08-14 11:55:33 +0300

    Fix bug
     Replace res.send().json() with res.status().json()

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
 
 import User from '../model/User.mode';
 
@@ -8,29 +9,29 @@ class UserController {
     res.status(201).json({ user });
   };
 
-  public getUsers = async (_: Request, res: Response) => {
+  public getUsers = asyncHandler(async (_: Request, res: Response) => {
     let user = await User.find();
-    if (!user) res.send(404).json({ message: 'No users found' });
+    if (!user) res.status(404).json({ message: 'No users found' });
     res.status(200).json({ user });
-  };
+  });
 
-  public getUser = async (req: Request, res: Response) => {
+  public getUser = asyncHandler(async (req: Request, res: Response) => {
     let user = await User.findById(req.params.id);
-    if (!user) res.send(404).json({ message: 'No user found' });
+    if (!user) res.status(404).json({ message: 'No user found' });
     res.status(200).json({ user });
-  };
+  });
 
-  public updateUser = async (req: Request, res: Response) => {
+  public updateUser = asyncHandler(async (req: Request, res: Response) => {
     let user = await User.findByIdAndUpdate(req.params.id, req.body);
-    if (!user) res.send(404).json({ message: 'No user found' });
+    if (!user) res.status(404).json({ message: 'No user found' });
     res.status(200).json({ user });
-  };
+  });
 
-  public deleteUser = async (req: Request, res: Response) => {
+  public deleteUser = asyncHandler(async (req: Request, res: Response) => {
     let user = await User.findByIdAndDelete(req.params.id);
-    if (!user) return res.send(404).json({ message: 'No user found' });
+    if (!user) res.status(404).json({ message: 'No user found' });
     res.status(200).json({ user });
-  };
+  });
 }
 
 export default UserController;
