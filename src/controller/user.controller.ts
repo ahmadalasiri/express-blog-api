67881d6be2f4071ddcf942f341d7671c74c389c9
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-08-19 17:00:45 +0300

    Refactor error handling in UserController

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,36 +1,36 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
 
+import HttpException from '../exceptions/HttpException';
 import User from '../model/User.mode';
 
 class UserController {
-  public createUser = async (req: Request, res: Response) => {
+  public createUser = asyncHandler(async (req: Request, res: Response) => {
     let user = await User.create(req.body);
     res.status(201).json({ user });
-  };
+  });
 
   public getUsers = asyncHandler(async (_: Request, res: Response) => {
     let user = await User.find();
-    if (!user) res.status(404).json({ message: 'No users found' });
     res.status(200).json({ user });
   });
 
-  public getUser = asyncHandler(async (req: Request, res: Response) => {
+  public getUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     let user = await User.findById(req.params.id);
-    if (!user) res.status(404).json({ message: 'No user found' });
+    if (!user) return next(new HttpException(404, 'No user found'));
     res.status(200).json({ user });
   });
 
-  public updateUser = asyncHandler(async (req: Request, res: Response) => {
-    let user = await User.findByIdAndUpdate(req.params.id, req.body);
-    if (!user) res.status(404).json({ message: 'No user found' });
+  public updateUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    let user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!user) return next(new HttpException(404, 'No user found'));
     res.status(200).json({ user });
   });
 
-  public deleteUser = asyncHandler(async (req: Request, res: Response) => {
+  public deleteUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     let user = await User.findByIdAndDelete(req.params.id);
-    if (!user) res.status(404).json({ message: 'No user found' });
-    res.status(200).json({ user });
+    if (!user) return next(new HttpException(404, 'No user found'));
+    res.status(204);
   });
 }
 
