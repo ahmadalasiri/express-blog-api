dc542939b37d09a79f59df4d95fa1175aecfa507
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-08-25 01:24:58 +0300

    Imp auth route

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import { AuthController } from '../controller';
+import { Routes } from '../interfaces/routes.interface';
+import { signupValidator } from '../middleware/validation';
+
+// import { loginValidator } from './../middleware/validation/auth.validator';
+
+export class AuthRoute implements Routes {
+  public path = '/auth';
+  public router = Router();
+  public authController = new AuthController();
+
+  constructor() {
+    this.initializerRoutes();
+  }
+
+  private initializerRoutes() {
+    this.router.post(`${this.path}/signup`, signupValidator, this.authController.signup);
+    // this.router.post(`${this.path}/login`, loginValidator, this.authController.login);
+  }
+}
